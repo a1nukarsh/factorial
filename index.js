c839f7984570989056f125d1b5dab815f5155648
Anukarsh <anukarshr@gmail.com>
2024-06-27 20:13:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 console.log("HJello")
 let a = 6
 
-function factorial(number){
-    let abcd= Array.from(Array(number+1).keys())
-    console.log(abcd.slice(1))
+// function factorial(number){
+//     let abcd= Array.from(Array(number+1).keys())
+//     // console.log(abcd.slice(1))
+//     let c = abcd.slice(1).reduce((a,b)=>{
+//         return a*b
+//     });
+//     console.log(c)
+//     // return c
+// }
+// factorial(5)
+
+function facfor(number){
+    let f=1
+    for (let i = 1; i <= number; i++) {
+        f = f*i;
+    }
+    return f
 }
-factorial(a)
+
+console.log(facfor(6))
